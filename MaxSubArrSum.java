d9c3df19ffa2a9d036da7e8a4c1adfb3ba04af44
Bhupendra Patil <bhupendrarajput1232@gmail.com>
2025-10-22 19:48:42 +0530

    kadane's algo

--- MaxSubArrSum.java
+++ MaxSubArrSum.java
@@ -21,8 +21,22 @@ public class MaxSubArrSum {
         }
         System.out.println("max sum = " + maxSum);
     }
+
+    public static void kadanes(int numbers[]){
+        int ms = Integer.MIN_VALUE;
+        int cs = 0;
+
+        for(int i=0; i<numbers.length; i++){
+            cs = cs + numbers[i];
+            if(cs < 0){
+                cs = 0;
+            }
+            ms = Math.max(cs, ms);
+        }
+        System.out.println("our max sub array sum is :" + ms);
+    }
     public static void main(String args[]){
-        int numbers[] = {2,4,6,8,10};
-        maxSubarraySum(numbers);
+        int numbers[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+        kadanes(numbers);
     }
 }
